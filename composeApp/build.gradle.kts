74c541364bdba31af02928974f7ea33c2b6b9061
Kiran Kumar H S <56409123+kirankumarhs29@users.noreply.github.com>
2025-07-22 11:38:39 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -156,6 +156,12 @@ android {
                 mappingFileUploadEnabled = true
             }
         }
+        getByName("debug") {
+            isMinifyEnabled = false
+            debug {
+                isDebuggable = true
+            }
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
